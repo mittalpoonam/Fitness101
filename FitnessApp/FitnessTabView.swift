23d175aa8964be10f05c08ae27d4d8d17bd42043
Poonam Mittal <poonammittal246@gmail.com>
2024-07-26 23:14:02 +0530

    Home header and progress circle

---
+++ FitnessApp/FitnessTabView.swift
@@ -0,0 +1,47 @@
+//
+//  FitnessTabView.swift
+//  FitnessApp
+//
+//  Created by poonam mittal on 26/07/24.
+//
+
+import SwiftUI
+
+struct FitnessTabView: View {
+    @State var selectedTab = "Home"
+    
+    init() {
+        let appearance = UITabBarAppearance()
+        
+        appearance.configureWithOpaqueBackground()
+        appearance.stackedLayoutAppearance.selected.iconColor = .green
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        
+    }
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            HomeView()
+                .tag("Home")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            HistoricDataView()
+                .tag("Historic")
+                .tabItem {
+                    Image(systemName: "chart.line.uptrend.xyaxis")
+                    Text("Charts")
+                }
+            
+            
+        }
+    }
+}
+
+struct FitnessTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FitnessTabView()
+    }
+}
