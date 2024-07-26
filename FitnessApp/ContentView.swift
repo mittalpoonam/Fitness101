1642a7c2c3a389e1b330abc4832278456359254d
Poonam Mittal <poonammittal246@gmail.com>
2024-07-26 21:05:26 +0530

    Initial Commit

---
+++ FitnessApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FitnessApp
+//
+//  Created by poonam mittal on 26/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by poonam mittal on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
